16c310a9d2b15c69d42b009ecab3bbdf452cf995
ibrahim.khalili <ibrahim.khalili@780296c5-5f3b-0410-9d2c-67838565597f>
2008-04-25 09:35:50 +0000

    messo file connessione iniziale ai peer vicini

---
+++ TorTella/src/init.h
@@ -0,0 +1,42 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
+ */
+ 
+#ifndef INIT_H
+#define INIT_H
+
+#include <glib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+struct init_data{
+	
+	char *ip;
+	char *port;
+	
+};
+typedef struct init_data init_data;
+
+GList *init_list;
+
+GList *init_read_file(const char *filename);
+init_data *init_char_to_initdata(char *buffer);
+
+#endif
