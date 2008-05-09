75924dd7e7de4dd252397c0599c042db23d98fdf
notargiacomo.s <notargiacomo.s@780296c5-5f3b-0410-9d2c-67838565597f>
2008-05-09 22:16:08 +0000

    Logger completato e perfettamente funzionante

--- trunk/TorTella/src/logger.c
+++ trunk/TorTella/src/logger.c
@@ -17,21 +17,19 @@
 #include "logger.h"
 
 int logger_init(int verbose_level, const char *filename) {
-	int fd;
 	char *pathname = (char*)malloc(strlen(PATH)+strlen(filename));
 	strcpy(pathname, PATH);
 	strcat(pathname, filename);
 	printf("[logger_init]init\n");
-	fd = open(pathname, O_CREAT|O_APPEND|O_WRONLY, S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH);
-	//close(fd);
-	logger_fd = fd;
+
+	fd_file = fopen(pathname, "a");
 	pthread_mutex_init(&logger_mutex, NULL);
 	verbose = verbose_level;
-	return fd;
+	return 1;
 }
 
 int logger_close() {
-	return close(logger_fd);
+	return fclose(fd_file);
 }
 
 char *get_timestamp() {
@@ -45,70 +43,16 @@ char *get_timestamp() {
 }
 
 int logger(int type, const char* text, ...) {
-	
-	char *timestamp = get_timestamp();
-	char *buffer = (char*)malloc(6000); //FIXIT
-	char *temp = (char*)malloc(4000);
-
-	sprintf(buffer, "%s",timestamp);
 	va_list ap;
 	va_start(ap, text);
-	vsprintf(temp, text, ap);
-	va_end(ap);
-	
-	strcat(buffer, temp);
-	int len = strlen(buffer);
-	
 	pthread_mutex_lock(&logger_mutex);
-	if(write(logger_fd, buffer, len)<0) {
-		fprintf(stderr, "Errore nella scrittura nel file\n");
-		return -1;
-	}
+	fprintf(fd_file, "%s", get_timestamp());
+	vfprintf(fd_file, text, ap);
 	pthread_mutex_unlock(&logger_mutex);
 	
 	if(type<=verbose)
-		printf(buffer);
-
-	return 0;
-}
-
-int logger_to(const char* filename, const char *text, ...) {
-	int fd;
-	char *pathname = (char*)malloc(strlen(PATH)+strlen(filename));
-	strcpy(pathname, PATH);
-	strcat(pathname, filename);
-	
-	if((fd=open(pathname, O_RDONLY|O_EXCL))<0) {
-		logger_init(8, filename);
-	}
-	close(fd);
-	
-	if((fd=open(pathname, O_APPEND|O_RDWR|O_EXCL))<0) {
-		fprintf(stderr, "Errore nell'apertura del file\n");
-		return -1;
-	}
-	
-	char *timestamp = get_timestamp();
-	char *buffer = (char*)malloc(2000); //FIXIT
-	char *temp = (char*)malloc(1000);
-	//strcpy(buffer, timestamp);
-	//strcat(buffer, text);
-	sprintf(buffer, "%s",timestamp);
-	//printf("[logger_add]text: %s\n", text);
-	//printf("[logger_add]%s\n", buffer);
-	//buffer[len] = '\n';
-	//buffer[len+1] = '\0';
-	va_list ap;
-	va_start(ap, text);
-	vsprintf(temp, text, ap);
+		vprintf(text, ap);
 	va_end(ap);
-	
-	strcat(buffer, temp);
-	int len = strlen(buffer);
-	if(write(fd, buffer, strlen(buffer))<0) {
-		fprintf(stderr, "Errore nella scrittura nel file\n");
-		return -1;
-	}
-	close(fd);
+
 	return 0;
 }
