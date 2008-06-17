403a449ed85b8aef522973bdffa397a50b78c149
ibrahim.khalili <ibrahim.khalili@780296c5-5f3b-0410-9d2c-67838565597f>
2008-06-17 15:48:53 +0000

    fatti i commenti

--- TorTella/src/socketmanager.h
+++ TorTella/src/socketmanager.h
@@ -1,7 +1,3 @@
-/*
- * Developed by Notargiacomo Simone
- * TorTella socket interface
- */
 #ifndef SOCKETMANAGER_H
 #define SOCKETMANAGER_H
 
@@ -20,13 +16,20 @@
 #define LP_WRITE    LP_NONE +1
 #define LP_READ     LP_NONE +2
 
-/*Crea un socket */
+/**
+ * Crea un socket di connessione remota ovvero
+ * si connette ad un server remoto.
+ */
 int create_tcp_socket(const char* dst_ip, int dst_port);
 
-/*Crea un socket di ascolto, server mode*/
+/**
+ * Crea un socket d'ascolto ovver un server TCP in attesa di connessioni.
+ */
 int create_listen_tcp_socket(const char* src_ip, int src_port);
 
-/*Chiude il socket, il socket viene mandato in TIME_WAIT*/
+/**
+ * Elimina un socket ovvero chiude una connessione, mettendo il socket nello stato di TIME_WAIT. 
+ */
 int delete_socket(int sock_descriptor);
 
 /*In ascolto per una nuova connessione.
@@ -38,26 +41,52 @@ int delete_socket(int sock_descriptor);
 */
 //int listen_packet(int listen_socket, char* buffer, unsigned int mode);
 //
+
+/**
+ * funzione che ritorna il nuovo descrittore di socket e l'indirizzo del client connesso,
+ * nel caso la chiamata di sistema accept() abbia avuto un esito positivo
+ */
 int listen_http_packet(int listen_socket);
 
-/*Legge il pacchetto in ingresso e ritorna il numero di caratteri*/
+/**
+ * Legge il pacchetto in ingresso e ritorna il numero di caratteri
+ */
 int recv_http_packet(int con_socket, char **buffer);
 
-/*Invia il contenuto di buffer*/
+/**
+ * Permette di scrivere sul socket connesso specificato dal relativo descrittore
+ */
 int send_packet(int sock_descriptor, char* buffer, int len);
 
-/*Attende la ricezione di un pacchetto (massima dimensione ricezione pari a BUFFER_LEN*/
+/**
+ * Attende la ricezione di un pacchetto, avente come dimensione massima quella pari al
+ * valore assunto dal paramtero BUFFER_LEN
+ */
 int recv_packet(int sock_descriptor,char** buffer);
 
-/*Attende la ricezione di un pacchetto con max_len byte*/
+/** 
+ * Attende la ricezione di un pacchetto, prefissando il valore massimo(max_len) di byte di
+ *  un blocco di dati del paccheto
+ */
 int recv_sized_packet(int sock_descriptor,char** buf, int max_len);
 
 //char *recv_http_packet(int sock_descriptor,char* buffer, int *len);
 
+/**
+ * Permette di ottenere l'indirizzo ip e il numero di porta del peer assocciato
+ * al socket, passato come parametro
+ */
 char *get_dest_ip(int socket);
 
+/**
+ * Permette di ottenere il numero di porta del peer assocciato
+ * al socket, passato come parametro
+ */
 u_int4 get_dest_port(int socket);
 
+/**
+ * Permette la chiusura asimmetrica di una connessione TCP
+ */
 int shutdown_socket(int sock_descriptor);
 
 #endif //SOCKETMANAGER_H
