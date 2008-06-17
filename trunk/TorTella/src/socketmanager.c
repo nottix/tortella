5bb2ef5c24a505570ef4eb20bba19eea05bd9469
ibrahim.khalili <ibrahim.khalili@780296c5-5f3b-0410-9d2c-67838565597f>
2008-06-17 15:48:53 +0000

    fatti i commenti

--- trunk/TorTella/src/socketmanager.c
+++ trunk/TorTella/src/socketmanager.c
@@ -29,7 +29,7 @@
 #include <string.h>
 #include <unistd.h>
 
-/*
+/**
  * Crea un socket di connessione remota ovvero
  * si connette ad un server remoto.
  */
@@ -43,29 +43,42 @@ int create_tcp_socket(const char *dst_ip, int dst_port)
 		logger(SOCK_INFO, "[create_tcp_socket]Errore nella creazione del socket tcp");
 		return -1;
 	}
+	//inizializzazione a 0 della struttura sockaddr_in
 	memset((char *) &sAddr, 0, sizeof(sAddr));
 	sAddr.sin_family = AF_INET;
 	if (dst_port > 0)
-		sAddr.sin_port = htons(dst_port);
+		/**
+		 * conversione dalla rappresentazione testuale/binaria dell'indirizzo 
+		 * al valore binario da inserire nella struttura sockaddr_in
+		 */ 
+		sAddr.sin_port = htons(dst_port); 
 	else {
 		logger(SOCK_INFO, "[create_tcp_socket]Errore nella porta: [%d]\n", dst_port);
 		return -1;
 	}
-
+	//Per convertire l' indirizzo IP dalla notazione puntata in formato ASCII al network byte order in formato binario
 	if (inet_pton(AF_INET, dst_ip, &sAddr.sin_addr) <= 0) {
 		logger(SOCK_INFO, "[create_tcp_socket]Errore nella conversione dell'indirizzo IP: [%s]\n", dst_ip);
 		return -1;
 	}
+	//funzione che permette al client TCP di aprire una connessione ad un server TCP
 	if (connect(sockfd, (struct sockaddr *) &sAddr, sizeof(sAddr)) < 0) {
 		logger(SOCK_INFO, "[create_tcp_socket]Unable to connect to: %s:%d\n", dst_ip, dst_port);
 		return -1;
 	}
 
 	int reuse = 1;
+	/*funzione che permette di impostare le caratteristiche 
 	if(setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) < 0) {
 		logger(SOCK_INFO, "[create_tcp_socket]Error in setsockopt SO_REUSEADDR\n");
 		return -1;
-	}
+	}*/
+	
+	/**
+	 * funzione che permette di impostare le caratteristiche del socket.
+	 * In questa funzione grazie all'utilizzo del paramtero SO_KEEPALIVE si 
+	 * ha la possibilità di gestire le persistenza delle connessioni. 
+	 */ 
 	if(setsockopt(sockfd, SOL_SOCKET, SO_KEEPALIVE, &reuse, sizeof(int)) < 0) {
 		logger(SOCK_INFO, "[create_tcp_socket]Error in setsockopt SO_KEEPALIVE\n");
 		return -1;
@@ -75,7 +88,7 @@ int create_tcp_socket(const char *dst_ip, int dst_port)
 }
 
 /*
- * Crea un socket d'ascolto ovver un server in attesa di connessioni.
+ * Crea un socket d'ascolto ovver un server TCP in attesa di connessioni.
  */
 int create_listen_tcp_socket(const char *src_ip, int src_port)
 {
@@ -86,37 +99,59 @@ int create_listen_tcp_socket(const char *src_ip, int src_port)
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Errore nella creazione del socket tcp");
 		return -1;
 	}
-
+	//inizializzazione a 0 della struttura sockaddr_in
 	memset((char *) &sAddr, 0, sizeof(sAddr));
 	sAddr.sin_family = AF_INET;
 	if (src_port > 0)
+		/**
+		 * conversione dalla rappresentazione testuale/binaria dell'indirizzo 
+		 * al valore binario da inserire nella struttura sockaddr_in
+		 */ 
 		sAddr.sin_port = htons(src_port);
 	else {
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Errore nella porta: [%d]\n", src_port);
 		return -1;
 	}
-
+	//Per convertire l' indirizzo IP dalla notazione puntata in formato ASCII al network byte order in formato binario
 	if (inet_pton(AF_INET, src_ip, &sAddr.sin_addr) <= 0) {
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Errore nella conversione dell'indirizzo IP: [%s]\n", src_ip);
 		return -1;
 	}
 
 	int reuse = 1;
+	/**
+	 * funzione che permette di impostare le caratteristiche del socket.
+	 * In questa funzione grazie all'utilizzo del paramtero SO_REUSEADDR si 
+	 * ha la possibilità di riutilizzare un indirizzo locale,modificando il comportamento
+	 * della bind che fallisce in caso l'indirizzo sia già in uso in un altro socket. 
+	 */
 	if(setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(int)) < 0) {
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Error in setsockopt SO_REUSEADDR\n");
 		return -1;
 	}
+	/**
+	 * funzione che permette di impostare le caratteristiche del socket.
+	 * In questa funzione grazie all'utilizzo del paramtero SO_KEEPALIVE si 
+	 * ha la possibilità di gestire le persistenza delle connessioni. 
+	 */
 	if(setsockopt(listenfd, SOL_SOCKET, SO_KEEPALIVE, &reuse, sizeof(int)) < 0) {
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Error in setsockopt SO_KEEPALIVE\n");
 		return -1;
 	}
 
-	// Binding
+	/**
+	 * funzione utilizzata per la fase di inizializzazione dell'indirizzo ip e del
+	 * numero di porta utilizzati dal socket, inoltre serve a far sapere al SO a quale
+	 * processo vanno inviati i dati ricevuti dalla rete.
+	 */
 	if ((bind(listenfd, (struct sockaddr *) &sAddr, sizeof(sAddr))) < 0) {
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Bind error\n");
 		return -1;
 	}
-	// Listen
+	/**
+	 * funzione utilizzata per porre il socket creato dallo stato di CLOSED a quello
+	 * di LISTEN, specificando il numero di connessioni che possono essere accettate dal server
+	 */
 	if (listen(listenfd, conf_get_qlen()) < 0) {
 		logger(SOCK_INFO, "[create_listen_tcp_socket]Listen error\n");
 		return -1;
@@ -125,8 +160,8 @@ int create_listen_tcp_socket(const char *src_ip, int src_port)
 	return listenfd;
 }
 
-/*
- * Elimina un socket ovvero chiude una connessione.
+/**
+ * Elimina un socket ovvero chiude una connessione,mettendo il socket nello stato di TIME_WAIT. 
  */
 int delete_socket(int sock_descriptor)
 {
@@ -135,18 +170,21 @@ int delete_socket(int sock_descriptor)
 		return -1;
 	}
 
-	// if( shutdown(sock_descriptor, SHUT_RDWR) < 0)
+	// funzione utilizzata per permettere la chiusura attiva della connessione identificata dal descrittore
 	if (close(sock_descriptor) < 0) {
 		logger(SOCK_INFO, "[delete_socket]Socket shutdown error");
 		return -1;
 	}
 	return 0;
 }
-
+/**
+ * funzione che ritorna il nuovo descrittore di socket e l'indirizzo del client connesso,
+ * nel caso la chiamata di sistema accept() abbia avuto un esito positivo
+ */
 int listen_http_packet(int listen_socket)
 {
 	int connFd = 0;
-
+	//permette  al server di prendere dal backlog la prima connessione completato sul socket specificato
 	if ((connFd=accept(listen_socket, (struct sockaddr *) NULL, NULL)) < 0) {
 		logger(SOCK_INFO, "[listen_http_packet]Errore nell'accept\n");
 		return -1;
@@ -155,6 +193,9 @@ int listen_http_packet(int listen_socket)
 	return connFd;
 }
 
+/**
+ * Legge il pacchetto in ingresso e ritorna il numero di caratteri
+ */
 int recv_http_packet(int connFd, char **buffer) {
 	int len = 0;
 	if ((len=recv_packet(connFd, buffer)) < 0) {
@@ -168,7 +209,9 @@ int recv_http_packet(int connFd, char **buffer) {
 
 	return len;
 }
-
+/**
+ * Permette di scrivere sul socket connesso specificato dal relativo descrittore
+ */
 int send_packet(int sock_descriptor, char *buffer, int len)
 {
 	int char_write = 0;
@@ -190,11 +233,19 @@ int send_packet(int sock_descriptor, char *buffer, int len)
 	return char_write;
 }
 
+/**
+ * Attende la ricezione di un pacchetto, avente come dimensione massima quella pari al
+ * valore assunto dal paramtero BUFFER_LEN
+ */
 int recv_packet(int sock_descriptor, char **buffer)
 {
 	return recv_sized_packet(sock_descriptor, buffer, conf_get_buffer_len());
 }
 
+/** 
+ * Attende la ricezione di un pacchetto, prefissando il valore massimo(max_len) di byte di
+ *  un blocco di dati del paccheto
+ */
 int recv_sized_packet(int sock_descriptor, char **buf, int max_len)
 {
 	u_int4 char_read = 0;
@@ -250,44 +301,43 @@ int recv_sized_packet(int sock_descriptor, char **buf, int max_len)
 	return len;
 }
 
+/**
+ * Permette di ottenere l'indirizzo ip del peer assocciato
+ * al socket, identificato dal descrittore passato come parametro
+ */
 char *get_dest_ip(int socket) {
+	
 	struct sockaddr_in peer;
 	memset((char *) &peer, 0, sizeof(peer));
 	u_int4 peer_len;
-	/* We must put the length in a variable.              */
-	peer_len = sizeof(peer);
-	/* Ask getpeername to fill in peer's socket address.  */
+   	peer_len = sizeof(peer);
 	if (getpeername(socket, (struct sockaddr *)&peer, &peer_len) == -1) {
 		logger(SOCK_INFO, "[get_dest_ip]Failed\n");
 		return NULL;
-	}
-
-	/* Print it. The IP address is often zero because     */
-	/* sockets are seldom bound to a specific local       */
-	/* interface.                                         */
+	}                                       
 	logger(SOCK_INFO, "[get_dest_ip]Peer's IP address is: %s\n", inet_ntoa(peer.sin_addr));
 	return inet_ntoa(peer.sin_addr);
 }
-
+/**
+ * Permette di ottenere il numero di porta del peer assocciato
+ * al socket, identificato dal descrittore passato come parametro
+ */
 u_int4 get_dest_port(int socket) {
+	
 	struct sockaddr_in peer;
 	memset((char *) &peer, 0, sizeof(peer));
 	u_int4 peer_len;
-	/* We must put the length in a variable.              */
 	peer_len = sizeof(peer);
-	/* Ask getpeername to fill in peer's socket address.  */
 	if (getpeername(socket, (struct sockaddr *)&peer, &peer_len) == -1) {
 		logger(SOCK_INFO, "[get_dest_port]Failed\n");
 		return -1;
 	}
-
-	/* Print it. The IP address is often zero because     */
-	/* sockets are seldom bound to a specific local       */
-	/* interface.                                         */
 	logger(SOCK_INFO, "[get_dest_port]Peer's port is: %d\n", (int) ntohs(peer.sin_port));
 	return (u_int4) ntohs(peer.sin_port);
 }
-
+/**
+ * Permette la chiusura asimmetrica di una connessione TCP
+ */
 int shutdown_socket(int sock_descriptor)
 {
 	if (sock_descriptor < 0) {
@@ -295,7 +345,7 @@ int shutdown_socket(int sock_descriptor)
 		return -1;
 	}
 
-	// if( shutdown(sock_descriptor, SHUT_RDWR) < 0)
+	
 	if (shutdown(sock_descriptor, SHUT_RDWR) < 0) {
 		logger(SOCK_INFO, "[shutdown_socket]Socket shutdown error");
 		return -1;
