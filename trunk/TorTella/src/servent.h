a7959d74ad499710559e498e0619c90e89ff008e
notargiacomo.s <notargiacomo.s@780296c5-5f3b-0410-9d2c-67838565597f>
2008-06-17 13:34:22 +0000

    Commenti

--- trunk/TorTella/src/servent.h
+++ trunk/TorTella/src/servent.h
@@ -13,6 +13,22 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
  */
+
+/**
+ * \file Servent
+ *
+ * \brief Gestisce il comportamento dei peer
+ *
+ * Crea le connessioni tra peer, gestisce l'invio e la ricezione dei pacchetti.
+ *
+ * \author $Author: Simone Notargiaco, Lorenzo Fortunato, Ibrahim Khalili $
+ *
+ * \version $Revision: 0.1 $
+ *
+ * \date $Date: 2008/06/18 14:16:20 $
+ *
+ * Contact: notargiacomo.s@gmail.com
+ */
  
 #ifndef SERVENT_H
 #define SERVENT_H
@@ -82,7 +98,7 @@ struct servent_data {
 };
 typedef struct servent_data servent_data;
 
-static GHashTable *servent_hashtable;
+static GHashTable *servent_hashtable;	//Hashtable di servent_data, una per ogni peer
 
 //---Rounting Hashtables-----
 
@@ -94,19 +110,21 @@ static GHashTable *leave_packet_hashtable = NULL;
 
 //---------------------------
 
-static servent_data *local_servent;
+static servent_data *local_servent;		//Dati del peer locale
 
-static u_int8 new_connection_counter = 10000;
+static u_int8 new_connection_counter;	//Limite inferiore generazione ID
 
-static pthread_t *timer_thread;
+static pthread_t *timer_thread;			//identificatore timer thread;
 
-static GList *client_fd;
-static GList *server_fd;
-static GList *server_connection_fd;
+static GList *client_fd;				//Lista client socket
+static GList *server_fd;				//Lista server socket in attesa di connessioni
+static GList *server_connection_fd;		//Lista server socket connessi
 
-static GList *client_thread;
-static GList *server_thread;
-static GList *server_connection_thread;
+static GList *client_thread;			//Lista identificatori client thread
+static GList *server_thread;			//Lista identificatori server thread in attesa di connessioni
+static GList *server_connection_thread;	//Lista identificatori server thread connessi
+
+//Macro di utilità
 
 #define WLOCK(servent)			logger(SYS_INFO, "[WLOCK]Try locking %lld\n", servent); \
 								if(servent_get(servent)!=NULL) { \
@@ -129,78 +147,199 @@ static GList *server_connection_thread;
 #define UNLOCK_F(servent)		pthread_rwlock_unlock( &(((servent)->rwlock_data)) ); \
 								logger(SYS_INFO, "[UNLOCK_F]Unlock %lld\n", servent->id);
 
+//Tale macro viene utilizzata per copiare un servent_data in un altro
 #define COPY_SERVENT(servent, copy)			copy=calloc(1, sizeof(servent_data)); \
 											memcpy(copy, servent, sizeof(servent_data))
 
-//Crea un server socket
+/**
+ * Crea un server socket
+ */
 int servent_create_server(char *src_ip, u_int4 src_port);
 
-//Crea un client socket
+/**
+ * Crea un client socket
+ */
 int servent_create_client(char *dst_ip, u_int4 dst_port);
 
+/**
+ * Avvia il server thread che viene usato per accettare
+ * le nuove connessioni e quindi lanciare nuovi thread
+ * per gestirle
+ */
 int servent_start_server(char *local_ip, u_int4 local_port);
 
+/**
+ * Avvia un client thread usato per gestire tutte le richieste di invio
+ * pacchetti al peer associato.
+ * 
+ * \param id Se si conosce l'id del peer a cui connettersi lo si specifica, altrimenti 0.
+ */
 servent_data *servent_start_client(char *dest_ip, u_int4 dest_port, u_int8 id);
 
+/**
+ * Questa funzione viene utilizzata per il boot iniziale, in quanto si connette
+ * alla lista dei peer conosciuti, presi dal file di init. Inoltre inizializza
+ * tutte le variabili necessarie e avvia il server di ascolto nuove connessioni.
+ */
 int servent_start(GList *init_servent);
 
+/**
+ * Tale funzione non fa altro che avviare il timer thread utilizzato per supportare
+ * il meccanismo di failure detection.
+ */
 int servent_start_timer(void);
 
+/**
+ * Si connette alla lista dei peer specificati, se qualcuno non è disponibile lo salta.
+ */
 int servent_init_connection(GList *init_servent);
 
+/**
+ * Questa funzione viene chiamata alla chiusura di TorTella, serve per chiudere
+ * tutti i socket aperti (non brutalmente).
+ */
 void servent_close_all(void);
 
-void kill_all_thread(int sig);
+/**
+ * Ultima funzione chiamata alla chiusura del programma, termina tutti i thread attivi.
+ */
+void servent_kill_all_thread(int sig);
 
+/**
+ * Inizializza le variabili del peer locale
+ */
 int servent_init(char *ip, u_int4 port, u_int1 status);
 
+/**
+ * Funzione utilizzata per il recupero delle chat conosciute da file,
+ * attualmente non più utilizzata.
+ */
 void servent_init_supernode();
 
+/**
+ * Funzione utilizzata per il salvataggio delle chat conosciute su file,
+ * attualmente non più utilizzata.
+ */
 void servent_close_supernode();
 
+//-----Gestione servent_data-----
+
+/**
+ * Restituisce il servent_data associato all'id richiesto.
+ */
 servent_data *servent_get(u_int8 id);
 
+/**
+ * Restituisce la lista completa delle servent_data.
+ */
 GList *servent_get_values(void);
 
+/**
+ * Restituisce la lista completa delle chiavi associate alle servent_data.
+ */
 GList *servent_get_keys(void);
 
+/**
+ * Restituisce la servent_data del peer locale.
+ */
 servent_data *servent_get_local(void);
 
+//-----Gestione Queue-----
+
+/**
+ * Aggiunge alla coda di pacchetti da inviare ad uno specifico peer.
+ * In particolare si passa una servent_data contenete tutti i dati
+ * necessari all'invio del pacchetto.
+ */
 void servent_send_packet(servent_data *sd);
 
+/**
+ * Rimuove dalla coda di pacchetti da inviare ad uno specifico peer.
+ * Se non ci sono pacchetti da rimuovere rimane in attesa.
+ */
 servent_data *servent_pop_queue(servent_data *sd);
 
+/**
+ * Appende alla coda di risposta di uno specifico peer.
+ * In particolare aggiunge la risposta ricevuta dopo l'invio di un pacchetto.
+ */
 void servent_append_response(servent_data *sd, const char *response);
 
+/**
+ * Rimuove dalla coda di risposta un elemento se presente,
+ * nel frattempo avvia un timer per rilevare eventuali timeout di risposta.
+ */
 char *servent_pop_response(servent_data *sd);
 
-void servent_add_to_chat_list(servent_data *sd, chat *chat_elem);
-
+/**
+ * Pulisce la lista dei pacchetti ricevuti che serve per scartare
+ * pacchetti con ID uguale a quelli ricevuti recentemente.
+ */
 void servent_flush_data(void);
 
-//-----Routing-------------
+//-----Routing-----
 
+/**
+ * Ritorna l'ID del pacchetto richiesto, se presente.
+ * E' una sorta di verifica presenta pacchetto.
+ * Utilizzato per gestire i duplicati.
+ */
 char *servent_get_search_packet(u_int8 id);
 
+/**
+ * Aggiunge il pacchetto alla lista dei pacchetti ricevuti.
+ */
 void servent_new_search_packet(u_int8 id);
 
+/**
+ * Ritorna l'ID del pacchetto richiesto, se presente.
+ * E' una sorta di verifica presenta pacchetto.
+ * Utilizzato per gestire i duplicati.
+ */
 char *servent_get_join_packet(u_int8 id);
 
+/**
+ * Aggiunge il pacchetto alla lista dei pacchetti ricevuti.
+ */
 void servent_new_join_packet(u_int8 id);
 
+/**
+ * Ritorna l'ID del pacchetto richiesto, se presente.
+ * E' una sorta di verifica presenta pacchetto.
+ * Utilizzato per gestire i duplicati.
+ */
 char *servent_get_leave_packet(u_int8 id);
 
+/**
+ * Aggiunge il pacchetto alla lista dei pacchetti ricevuti.
+ */
 void servent_new_leave_packet(u_int8 id);
 
-//-----THREAD--------------
+//-----THREAD-----
 
-//Thread che riceve i pacchetti e risponde adeguatamente
+/**
+ * Thread che riceve le richieste di connessione e lancia un thread (servent_responde)
+ * per gestire tali richieste.
+ */
 void *servent_listen(void *parm);
 
+/**
+ * Server thread che riceve i pacchetti e risponde adeguatamente. Ne esiste uno per ogni
+ * peer a cui si è connessi. Questa funzione è il vero cuore di TorTella,
+ * infatti gestisce tutti i comportamente dei programma in base ai pacchetti ricevuti.
+ */
 void *servent_responde(void *parm);
 
+/**
+ * Client thread utilizzato per gestire il canale di invio pacchetti ad un peer.
+ */
 void *servent_connect(void *parm);
 
+/**
+ * Thread utilizzato per gestire il meccanismo di failure detection e per pulire
+ * la lista dei pacchetti ricevuti. L'intervallo di tempo è impostato nel file
+ * di configurazione.
+ */
 void *servent_timer(void *parm);
 
 #endif //SERVENT_H
